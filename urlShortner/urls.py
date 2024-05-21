52662cf25adaeeddb64f13f1c28d3b3d354040ce
TUSHAR GOEL <tushargoel66@gmail.com>
2024-05-21 15:06:30 +0530

    bug

--- urlShortner/urls.py
+++ urlShortner/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path,include
 from django.views.generic import TemplateView
 from Services.views import redirect_to_target_page
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("",TemplateView.as_view(template_name = "home.html")),
